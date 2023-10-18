59b8a3b50683ac9810b4938717bb144145ba9ca4
Nikita Korovin <n.koroffin@gmail.com>
2023-10-18 21:02:23 +0400

    initial commit

---
+++ src/DeleteButton.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import IconButton from '@mui/material/IconButton';
+
+export const DeleteButton = styled(IconButton)`
+  position: absolute;
+  right: 0;
+  top: 0;
+`;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./Box";
+export * from "./DeleteButton";
+export * from "./FormInputProps";
+export * from "./FormInputText";
+export * from "./FormInputSelect";
